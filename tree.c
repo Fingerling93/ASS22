bcb45d24b783ed4bd09d6a7aa786f87db98d58f8
Fingerling93 <carski.nikola@gmail.com>
2017-12-03 15:44:00 +0100

    new tree

--- tree.c
+++ tree.c
@@ -22,39 +22,76 @@ typedef struct Story
 struct Story * initTree(char*,char*,char*,char*);
 struct Story * addRight(struct Story * root,char*,char*,char*,char*);
 struct Story * addLeft(struct Story * root,char*,char*,char*,char*);
+void printStory(struct Story *node);
+void printStories(struct Story *head);
 
 
 int main()
 {
 
 		struct Story *root = initTree("Chapter 1", "Chapter 2", "Chapter 3", "text");
+
 		struct Story **head = (struct Story **) malloc(sizeof( struct Story *));
 		
 		if(head == NULL)
 		{
 			printf("memory not allocated\n");
 		}
-		head = &root;
-		struct Story *current = *head;
+		*head = root;
+
+		struct Story *current = (*head)->right;
 
 		current = addRight(current, "Chapter 2", "Chapter 4", "Chapter 5", "text");
 		current = addLeft(current, "Chapter 3", "Chapter 6", "Chapter 7", "text");
-
-		current = *head->right; //(  ERROR: head is a pointer...)
-
+	
+		current = (*head)->right; 
+		//printStory(current);
+		
 		current = addRight(current, "Chapter 4", "Chapter 8", "Chapter 9", "text");
 		current = addLeft(current, "Chapter 5", "Chapter 10", "Chapter 11", "text");
-
-		current = *head->left;  // ima error
+		//printStory(current->right);
+		//printStory(current->left);
+		
+		
+		
+		current = (*head)->left; 
 
 		current = addRight(current, "Chapter 6", "Chapter 12", "Chapter 13", "text");
 		current = addLeft(current, "Chapter 7", "Chapter 14", "Chapter 15", "text");
+		
+		printStories(*head);
 
-		//free(story);
+		free(root);
 		free(head);
 		//free(right);
 		//for(left);
+		
+		return 0;
 
+}
+
+void printStories(struct Story *head)
+{
+	if(head == NULL)
+	{
+		printf("prazna lista");
+		return;
+	}
+	if(head->right != NULL)
+		printStories(head->right);
+	
+	if(head->left != NULL)
+		printStories(head->left);
+	
+	printf("%s\n", head->title);
+}
+
+void printStory(struct Story * node)
+{
+	printf("%s\n", node->title);
+	printf("%s\n", node->file_a);
+	printf("%s\n", node->file_b);
+	printf("%s\n", node->text);
 }
 
 struct Story * addLeft(struct Story * current, char t[TITLE_MAX], char fa[PATH_MAX], char fb[PATH_MAX], char *text)
@@ -70,12 +107,13 @@ struct Story * addLeft(struct Story * current, char t[TITLE_MAX], char fa[PATH_M
 	}
 	strcpy(left->title,t); // bilo : left->title = t; ERROR 
 	left->text = text;
-	left->right = NULL;
-	left->left = NULL;
+	left->right = (struct Story *) malloc(sizeof(struct Story));
+	left->left = (struct Story *) malloc(sizeof(struct Story));
 	strcpy(left->file_a,fa);
 	strcpy(left->file_b,fb);
 
 	current->left = left;
+
 	return current;
 }
 
@@ -87,11 +125,11 @@ struct Story * initTree(char t[TITLE_MAX], char f_a[PATH_MAX], char f_b[PATH_MAX
 		printf("memory not allocted (initTree)\n");
 		return NULL;
 	}
-	strcpy(story->title, t);//story->title = t; ERROR
+	strcpy(story->title, t);
 	story->text = text;
 	story->right = story;
 	story->left = story;
-	strcpy(story->file_a,f_a); // bilo : story->file_a = f_a; ERROR
+	strcpy(story->file_a,f_a);
 	strcpy(story->file_b,f_b);
 
 	return story;
@@ -110,16 +148,14 @@ struct Story * addRight(struct Story *current, char t[TITLE_MAX], char fa[PATH_M
 
 	strcpy(right->title, t);
 	right->text = text;
-	right->right = NULL;
-	right->left = NULL;
+	right->right = (struct Story *) malloc(sizeof(struct Story));
+	right->left = (struct Story *) malloc(sizeof(struct Story));
 	strcpy(right->file_a,fa);
 	strcpy(right->file_b,fb);
 
 	current->right = right;
 
 	return current;
-
-
 }
 
 
